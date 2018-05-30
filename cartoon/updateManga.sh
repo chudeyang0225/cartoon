4b3acb5810cc6704cad936477b329f9de39da97e
deyangchu <deyang.chu@netwolves.com>
2018-05-30 13:42:31 -0400

    Clear directory

--- cartoon/updateManga.sh
+++ cartoon/updateManga.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 PATH=$PATH:/usr/local/bin
 export PATH
+
+if [ ! -f data.json ]; then
+    echo '{"comic": "", "filenum": "0", "eptitle": ""}' > data.json
+    echo "Edit config file!"
+    break
+fi
+
 rm -rf ./manga/*
 scrapy crawl comic
 python3 push2Kindle.py
